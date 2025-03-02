a902fd7994dd24f0b8a6b178b6cc4d8445bf5408
chanokin <chanokin@gmail.com>
2025-03-02 16:40:45 +0000

    remove warn

--- src/tbp/monty/frameworks/config_utils/default_paths.py
+++ src/tbp/monty/frameworks/config_utils/default_paths.py
@@ -1,21 +1,21 @@
 from pathlib import Path
-# todo integrate to a global logging
-from warnings import warn
+# # todo integrate to a global logging
+# from warnings import warn
 
 def get_base_dir():
     _path = Path.home() / "monty_experiments"
-    warn(f"Using default base path {_path}")
+    # warn(f"Using default base path {_path}")
     _path.mkdir(exist_ok=True)
     return _path
 
 def get_log_dir():
     _path = get_base_dir() /  "logs"
-    warn(f"Using default logs path {_path}")
+    # warn(f"Using default logs path {_path}")
     _path.mkdir(exist_ok=True)
     return _path
 
 def get_data_dir():
     _path = get_base_dir() / "data"
-    warn(f"Using default data path {_path}")
+    # warn(f"Using default data path {_path}")
     _path.mkdir(exist_ok=True)
     return _path
